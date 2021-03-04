3f5511b906adbba275443b9c6721e1932f0d6824
suyeon <tndus7644@gmail.com>
2021-03-04 21:59:53 +0900

    initcommit

---
+++ src/view/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const App = () => {
+
+    return(
+        <Container>
+            App
+        </Container>
+    )
+}
+
+const Container = styled.div`
+
+`;
+
+export default App;
